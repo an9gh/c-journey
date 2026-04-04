3ac2fa51d810ae77235d97bfd8ac72fe35fb26f6
anakh <dev.an9gh@gmail.com>
2026-04-05 01:23:16 +0530

    phase1: unit converter - celsius, km, kg, conversions

--- phase1-foundations/converter.c
+++ phase1-foundations/converter.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 int main(){
-	// F = (C × 9/5) + 32
-	float celsius=46, fahrenheit;
+	printf("=== Unit Converter ===\n");
+	// F = (C × 9/5) + 32(celsius to fehrenheit)
+	float celsius=46, fahrenheit,miles,pounds,kilogram=70,kilometer=10;
 	fahrenheit= (celsius*9/5)+32;
-	printf("conveting %.1f\u00B0C celsius ->  %.2f\u00B0F\n fahrenheit",celsius,fahrenheit);
+	printf("%.2f\u00B0C -> %.2f\u00B0F\n ",celsius,fahrenheit);
+	
+	//miles = km × 0.621371(kilometer to miles)
+	miles = (kilometer * 0.621371);
+	printf("%.2f km -> %.2f miles \n",kilometer,miles);
+
+	//pounds = kg × 2.204629(kilograms to pounds)
+	pounds = (kilogram * 2.20462);
+	printf("%.2f kg -> %.2f pounds\n",kilogram,pounds);
+
+
 	return 0;
 }
