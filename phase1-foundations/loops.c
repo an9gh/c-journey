771b813b3e93981afda380e7f5e4282cfba8461f
anakh <dev.an9gh@gmail.com>
2026-04-06 23:39:55 +0530

    phase1: for-loop - multiplication table

---
+++ phase1-foundations/loops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+	//for-loop exersise
+	int num=9;
+	printf("== Multiplication Table of %d ==\n",num);
+	
+	for (int i = 1; i <=10; i++){
+		printf("%d x %d = %d\n",num,i,num*i);
+	}
+	
+
+
+
+	return 0;
+}
