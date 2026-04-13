9c79468a9fa3f3a402568b3ee0ca7dde40dd0be6
anakh <dev.an9gh@gmail.com>
2026-04-13 22:35:15 +0530

    phase1: find prime number - user input

---
+++ phase1-foundations/prime_number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+
+	int num;
+	int isPrime = 1;
+	printf("=== PRIME numbers ==\n");
+
+	printf("Enter Your Number: ");
+	scanf("%d",&num);
+	if (num <= 1){
+		isPrime = 0;
+	}
+	else{
+		for (int i = 2; i < num; i++){
+			if(num % i == 0){
+				isPrime = 0;
+				break;
+			}
+		}
+	}
+
+	if (isPrime == 1){
+		printf("%d is a prime number\n",num);
+	}
+	else{
+		printf("%d is not a prime number\n",num);
+	}
+
+
+
+
+
+
+
+	return 0;
+}
