20ee967390f884bd99dc9904d9a833b8af7c6b8f
anakh <dev.an9gh@gmail.com>
2026-04-20 20:58:58 +0530

    phase1: Add number pattern

--- phase1-foundations/pattern.c
+++ phase1-foundations/pattern.c
@@ -23,6 +23,13 @@ int main(){
 		printf("\n");
 	}
 
+	for (int row = 1; row <= n; row++) {
+		for (int col = 1; col <=row; col++) {
+			printf("%d ",col);
+		}
+		printf("\n");
+	}
+
 
 return 0;
 }
