33442db4deb48a1755a850cb4d64e7dbd3b5b1b9
anakh <dev.an9gh@gmail.com>
2026-04-24 20:27:12 +0530

    phase1: Update - functions,prototypes and scope

--- phase1-foundations/functions.c
+++ phase1-foundations/functions.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+
 void print_result(char *label,int value){
 	printf("%s: %d\n",label,value);
 }
 
 int add(int a,int b);
 
+int multiply(int a,int b);
+
+int square(int n);
+
 
 int main(){
 
-	print_result("Add", add(3, 5));
+	print_result("Add", add(3, 5));	
+	print_result("multiply", multiply(3, 5));
+	print_result("square", square(5));
 
 	return 0;
 }
@@ -19,4 +26,10 @@ int add(int a,int b){
 	return a + b;
 }
 
+int multiply(int a,int b){
+	return a * b;
+}
 
+int square(int n){
+	return n * n;
+}
